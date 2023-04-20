82965d41c102486cec7e73a0f4d84d92c080538e
tkg613 <lyntallow@mail.com>
2023-04-20 13:13:23 +0900

    Add controllers, routes, models, and token

---
+++ backend/controllers/userController.js
@@ -0,0 +1,85 @@
+const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const User = require('../models/userModel')
+
+// @desc   Register a new user
+// @route  /api/users
+// @access Public
+const registerUser = asyncHandler( async function(req, res) {
+  
+  const {name, email, password} = req.body
+
+  // Validation
+  if (!name || !email || !password){
+    res.status(400)
+    throw new Error('Please include all fields.')
+  }
+
+  // Find if user already exists
+  const userExists = await User.findOne({email: email})
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists.')
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // Create user
+  const user = await User.create({
+    name: name,
+    email: email,
+    password: hashedPassword
+  })
+
+  if (user){
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id)
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data.')
+  }
+
+})
+
+// @desc   Login
+// @route  /api/users/login
+// @access Public
+const loginUser = asyncHandler(async function(req, res) {
+  const {email, password} = req.body
+
+  const user = await User.findOne({email: email})
+
+  // Check user exists & passwords match
+  if (user && (await bcrypt.compare(password, user.password))) {
+      res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id)
+      })
+  } else {
+    res.status(401)
+    throw new Error('Invalid credentials.')
+  }
+}) 
+
+// Generate token
+const generateToken = function(id) {
+  return jwt.sign({id}, process.env.JWT_SECRET, {
+    expiresIn: '30d'
+  })
+}
+
+module.exports = {
+  registerUser,
+  loginUser
+}
---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const colors = require('colors')
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 8000
+
+// Connect to db
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// Routes
+app.use('/api/users/', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, console.log('Server started on port', PORT))
